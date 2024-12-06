9b2b3fc7bd99fca9a3d9950107cb6284d640381a
Ryell Martinez <ryell.martinez@student.tdsb.on.ca>
2024-12-06 11:36:21 -0500

    context folder created

---
+++ src/context/index.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState, useContext } from "react";
+import { Map } from 'immutable';
+
+const StoreContext = createContext();
+
+export const StoreProvider = ({ children }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [cart, setCart] = useState(Map());
+
+    return (
+        <StoreContext.Provider value={{ email, setEmail,password,setPassword,firstName,setFirstName,lastName,setLastName, cart, setCart }}>
+            {children}
+        </StoreContext.Provider>
+    );
+}
+
+export const useStoreContext = () => {
+    return useContext(StoreContext);
+}
--- src/views/RegisterView.jsx
+++ src/views/RegisterView.jsx
@@ -2,6 +2,7 @@ import "./RegisterView.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function RegisterView() {
     const [firstName, setFirstName] = useState("");
@@ -9,23 +10,55 @@ function RegisterView() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [rePassword, setRePassword] = useState("");
+    const genres = [
+        { checked: false, genre: 'Action' },
+        { checked: false, genre: 'Adventure' },
+        { checked: false, genre: 'Animation' },
+        { checked: false, genre: 'Comedy' },
+        { checked: false, genre: 'Music' },
+        { checked: false, genre: 'Horror' },
+        { checked: false, genre: 'Mystery' },
+        { checked: false, genre: 'Fantasy' },
+        { checked: false, genre: 'Thriller' },
+        { checked: false, genre: 'Western' }
+    ];
+    const navigate = useNavigate();
+
+    function register(event){
+        event.preventDefault();
+        console.log(password);
+        if (password === rePassword) {
+            navigate(`/movie/genre/28`);
+        } else {
+            alert("passwords don't match");
+        }
+    }
 
     return (
         <div>
             <Header />
+            <div>
+                {
+                    genres.map((item, i) =>
+                        <div key={i}>
+                            <input type="checkbox" onChange={(e) => handleCheckBox(e, i)}>{item.genres}</input>
+                        </div>
+                    )
+                }
+            </div>
             <div className="register-container">
                 <h className="register-title">Join Us!</h>
-                <form className="register-form" onSubmit={(event) => { login(event) }}>
+                <form className="register-form" onSubmit={(event) => { register(event) }}>
                     <label className="register-text">First Name:</label>
                     <input type="text" className="register-inputs" value={firstName} onChange={(event) => { setFirstName(event.target.value) }} required />
                     <label className="register-text">Last Name:</label>
                     <input type="text" className="register-inputs" value={lastName} onChange={(event) => { setLastName(event.target.value) }} required />
                     <label className="register-text">Email:</label>
-                    <input type="text" className="register-inputs" value={email} onChange={(event) => { setEmail(event.target.value) }} required />
+                    <input type="email" className="register-inputs" value={email} onChange={(event) => { setEmail(event.target.value) }} required />
                     <label className="register-text">Password:</label>
-                    <input type="text" className="register-inputs" value={password} onChange={(event) => { setPassword(event.target.value) }} required />
+                    <input type="password" className="register-inputs" value={password} onChange={(event) => { setPassword(event.target.value) }} required />
                     <label className="register-text">Re-Enter Password:</label>
-                    <input type="text" className="register-inputs" value={rePassword} onChange={(event) => { setRePassword(event.target.value) }} required />
+                    <input type="password" className="register-inputs" value={rePassword} onChange={(event) => { setRePassword(event.target.value) }} required />
                     <button className="register-button">Sign Up</button>
                 </form>
             </div>
