f1c7f6edf6717c326227c3ecaa0a306f19994b08
ryell <ryell.martinez@student.tdsb.on.ca>
2024-12-11 22:30:03 -0500

    done

--- src/views/SettingsView.jsx
+++ src/views/SettingsView.jsx
@@ -1,8 +1,9 @@
 import { useStoreContext } from "../context";
 import "./SettingsView.css";
+import { useState, useEffect, useRef } from "react";
 
 function SettingsView() {
-    const { password, email, firstName, lastName, checked, toggleGenre } = useStoreContext();
+    const { password, email, firstName, lastName, checked, toggleGenre, setFirstName, setLastName } = useStoreContext();
     const genres = [
         'Action',
         'Adventure',
@@ -16,18 +17,30 @@ function SettingsView() {
         'Western'
     ];
 
+    function changeName(event) {
+        event.preventDefault();
+        alert("changed!");
+    }
+
     return (
         <div className="settings-view">
             <h1>Settings</h1>
             <div className="settings-info">
-                <p className="first-name">First Name: {firstName}</p>
-                <p className="last-name">Last Name: {lastName}</p>
-                <p className="email">Email: {email}</p>
-                <p className="password">Password: {password}</p>
+                <form className="settings-form" onSubmit={(event) => { changeName(event) }}>
+                    <label className="settings-text">First Name:</label>
+                    <input type="text" id="first-name" className="settings-inputs" value={firstName} onChange={(event) => { setFirstName(event.target.value) }} required />
+                    <label className="settings-text">Last Name:</label>
+                    <input type="text" id="last-name" className="settings-inputs" value={lastName} onChange={(event) => { setLastName(event.target.value) }} required />
+                    <button className="settings-button">Change First/Last Name?</button>
+                </form>
+                <label className="settings-text">Email:</label>
+                <input type="email" id="email" className="settings-inputs readOnly" value={email} required />
+                <label className="settings-text">Password:</label>
+                <input type="text" id="email" className="settings-inputs readOnly" value={password} required />
             </div>
-            <div className="genre-checklist">
+            <div className="settings-checklist">
                 <h className="genre-title">Genres</h>
-                <p className="genre-paragraph">Please choose up to 10 genres so we can personalize your account</p>
+                <p className="settings-paragraph">Please choose up to 10 genres so we can personalize your account</p>
                 {genres.map((item, i) => (
                     <div key={i}>
                         <input
@@ -36,7 +49,7 @@ function SettingsView() {
                             onChange={() => toggleGenre(item)} // Toggle the checked state of the genre
                             id={`checkbox-${i}`}
                         />
-                        <label className="genre-name">{item}</label>
+                        <label className="settings-name">{item}</label>
                     </div>
                 ))}
             </div>
